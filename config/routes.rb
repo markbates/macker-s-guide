472335893c9aebb93b3ac3262a05547bdb90f229
Mark Bates <mark@markbates.com>
2008-10-02 18:04:40 -0400

    Using assets_bundle and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Mack::Routes.build do |r|
     map.search "/search", :action => :search, :method => :post
     map.connect "/search", :action => :search, :method => :get
   end # searches
+  
+  r.with_options(:host => 'www.mackframework.com', :scheme => 'http', :port => '80') do |map|
+    map.mack_home '/'
+    map.mack_about '/about'
+    map.mack_archives '/archives'
+    map.mack_contribute '/contribute'
+    map.mack_support '/support'
+    map.mack_user_guide '/user-guide'
+  end
 
   r.home_page "/", :controller => :default, :action => :index
   
